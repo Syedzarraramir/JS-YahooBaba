9adb1aca692c151ea9c03fdd60336a4a9a888434
Syedzarraramir <szarraramir@gmail.com>
2024-10-10 17:24:09 +0500

    Array & String Methods Added

---
+++ stringMethod.js
@@ -0,0 +1,40 @@
+var string = "JavaScript";
+var a = string.length;
+//it will show the length
+console.log(a);
+
+//To Lower Case
+var string = "JavaScript";
+var b = string.toLowerCase();
+//it will show the length
+console.log(b);
+
+//To Upper Case
+var string = "JavaScript";
+var c = string.toUpperCase();
+//it will show the length
+console.log(c);
+
+//To Search Word
+var string = "Java Script";
+var d = string.includes("Script");
+//it will show the length
+console.log(d);
+
+//It Shows the Word Start With
+var string = "JavaScript";
+var e = string.startsWith("J");
+//it will show the length
+console.log(e);
+
+//It Show the word end with
+var string = "JavaScript";
+var f = string.endsWith("t");
+//it will show the length
+console.log(f);
+
+//It Return the Position
+var string = "JavaScript";
+var g = string.search("S");
+//it will show the length
+console.log(g);
