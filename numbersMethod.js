2f6c33dfc07558e39f4fceab641cbf7eedee7cb5
Syedzarraramir <szarraramir@gmail.com>
2024-10-11 14:32:54 +0500

    remaining work added successfully

--- numbersMethod.js
+++ numbersMethod.js
@@ -21,3 +21,14 @@ var r = Number.isInteger(integar1); // Should return false
 var s = Number.isInteger(integar2); // Should return true
 
 console.log(r + s); // This will print 1 because false (0) + true (1) = 1
+
+//toFixed();
+var numb = 123.456789;
+var fixedNum = numb.toFixed(2); // This will round the number to 2 decimal places
+console.log(fixedNum); // Output: "123.46"
+
+//toPrecision();
+var numbers = 5.5;
+var t = numbers.toPrecision(1);
+console.log(t);
+
