93841510210403517114b4302589a618e7c8a4c0
Syedzarraramir <szarraramir@gmail.com>
2024-10-11 15:31:23 +0500

    Math Method Added Successfully

---
+++ mathMethod.js
@@ -0,0 +1,71 @@
+//ceil();
+var number = 12.454
+var u = Math.ceil(number);
+console.log(u);
+
+//floor();
+ var v = Math.floor(number);
+ console.log(v);
+
+ //round();
+ var w = Math.round(number); //It will round to its nearest Integar
+ console.log(w);
+
+ //trunc();
+ var y = 122.89;
+ var x = Math.trunc(y);
+ console.log(x);
+
+
+//  var myName = "Zarrar";
+//  console.log(myName);
+//  var myName = "Owais"
+//  console.log(myName)
+
+
+//max();
+var z = Math.max(u,v,w,x,y);
+console.log(z);
+
+//min();
+min = Math.min(u,v,w,x,y);
+console.log(min);
+
+//sqrt();
+var sqrt = Math.sqrt(49);
+console.log(sqrt);
+
+//cbrt();
+var cbrt = Math.cbrt(900);
+console.log(cbrt);
+
+//pow();
+var pow = 43;
+power = Math.pow(pow,2);
+console.log(power);
+
+
+
+
+
+
+//random();
+var random = Math.random();
+value = Math.ceil(Math.random() * 6); 
+console.log(random);
+console.log(value);
+
+//abs();
+var abs = Math.abs(number);
+console.log(abs);
+
+//pi();
+var pi = Math.PI;
+console.log(pi);
+var toCheck = isFinite(pi);
+console.log(toCheck);
+
+//The End
+//11
+//October 
+//2024
