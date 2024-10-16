b2d68fa613f211d7f242510416c8c6ffc2a81b69
Syedzarraramir <szarraramir@gmail.com>
2024-10-16 16:10:45 +0500

    work addde

--- jqueryPractise.js
+++ jqueryPractise.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-var a = $('.myName').html();
-var b = $('.myName').text();
-var c = $('*').html();
-console.log(c);
-});
+    var a = $('.myName').html();
+    var b = $('.myName').text();
+    var c = $('*').html();
 
+    var d = $("ul li:gt(2)").css("color", "red");
+
+    // Log each li element and its color property
+    $("ul li:gt(2)").each(function(index, element) {
+        console.log($(element).text() + ' has color: ' + $(element).css("color"));
+    });
+});
 
